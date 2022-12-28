6f305fd332fce3bd8e56cae498d5b56f5c09417b
Jay <jayhapaliya141206@gmail.com>
2022-12-28 17:19:39 +0530

    Java Clas

---
+++ java/Cmd.java
@@ -0,0 +1,12 @@
+package java;
+
+public class Cmd {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println("Argumenys are");
+            for (String x : args)
+                System.out.print(x + " ");
+        } else
+            System.out.println("No Arguments");
+    }
+}
