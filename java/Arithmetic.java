3454eb0cc522477cccd23f49355fdaf87830045d
Jay <jayhapaliya141206@gmail.com>
2022-12-28 17:33:09 +0530

    JAva Class

--- java/Arithmetic.java
+++ java/Arithmetic.java
@@ -1,4 +1,14 @@
-
 public class Arithmetic {
-    main
+    public static void main(String[] args) {
+        int x = 10, y = 20;
+        System.out.println(x + y);
+        System.out.println(x * y);
+        System.out.println(y / x);
+        int z = (x + y * 10);
+        System.out.println(z);
+        z = x++;
+        System.out.println(z + " " + x);
+        z = ++x;
+        System.out.println(z + " " + x);
+    }
 }
