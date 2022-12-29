4e5ea17835e00fa2967749d061d8fc70d3e7bc1c
Jay <jayhapaliya141206@gmail.com>
2022-12-29 10:44:36 +0530

    Java Class

--- Evenodd.java
+++ Evenodd.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 public class Evenodd {
     public static void main(String[] args) {
         System.out.println("Enter the number ");
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        if (n % 2 == 0) {
-            System.out.println(" Even Number is" + n);
-        } else {
-            System.out.println(" odd Number is" + n);
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            if (n % 2 == 0) {
+                System.out.println(" Even Number is" + n);
+            } else {
+                System.out.println(" odd Number is" + n);
+            }
         }
     }
 }
